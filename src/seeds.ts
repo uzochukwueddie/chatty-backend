af6a6e012a0d0a641b5827b466c29c37b5a52140
uzochukwueddie <uzo.odozi@gmail.com>
2022-08-20 22:00:51 +0200

    added seed data file

---
+++ src/seeds.ts
@@ -0,0 +1,79 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import dotenv from 'dotenv';
+import { faker } from '@faker-js/faker';
+import { floor, random } from 'lodash';
+import axios from 'axios';
+import { createCanvas } from 'canvas';
+
+dotenv.config({});
+
+function avatarColor(): string {
+  const colors: string[] = [
+    '#f44336',
+    '#e91e63',
+    '#2196f3',
+    '#9c27b0',
+    '#3f51b5',
+    '#00bcd4',
+    '#4caf50',
+    '#ff9800',
+    '#8bc34a',
+    '#009688',
+    '#03a9f4',
+    '#cddc39',
+    '#2962ff',
+    '#448aff',
+    '#84ffff',
+    '#00e676',
+    '#43a047',
+    '#d32f2f',
+    '#ff1744',
+    '#ad1457',
+    '#6a1b9a',
+    '#1a237e',
+    '#1de9b6',
+    '#d84315'
+  ];
+  return colors[floor(random(0.9) * colors.length)];
+}
+
+function generateAvatar(text: string, backgroundColor: string, foregroundColor = 'white') {
+  const canvas = createCanvas(200, 200);
+  const context = canvas.getContext('2d');
+
+  context.fillStyle = backgroundColor;
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  context.font = 'normal 80px sans-serif';
+  context.fillStyle = foregroundColor;
+  context.textAlign = 'center';
+  context.textBaseline = 'middle';
+  context.fillText(text, canvas.width / 2, canvas.height / 2);
+
+  return canvas.toDataURL('image/png');
+}
+
+async function seedData(count: number) {
+  let i = 0;
+  try {
+    for (i = 0; i < count; i++) {
+      const characterName: string = faker.unique(faker.word.adjective, [8]);
+      const color = avatarColor();
+      const avatar = generateAvatar(characterName.charAt(0).toUpperCase(), color);
+
+      const body = {
+        username: characterName,
+        email: faker.internet.email(),
+        password: 'qwerty',
+        avatarColor: color,
+        avatarImage: avatar
+      };
+      console.log(`***SEEDING DATABASE*** - ${i + 1} / ${count} - ${characterName}`);
+      await axios.post(`${process.env.API_URL}/signup`, body);
+    }
+  } catch (error: any) {
+    console.log(error?.response);
+  }
+}
+
+seedData(10);
