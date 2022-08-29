178df68ec0825605023255f5ad79005a214a39aa
uzochukwueddie <uzo.odozi@gmail.com>
2022-08-29 14:04:05 +0200

    remove handleExit method

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,6 @@ import express, { Express } from 'express';
 import { ChattyServer } from '@root/setupServer';
 import databaseConnection from '@root/setupDatabase';
 import { config } from '@root/config';
-import Logger from 'bunyan';
-
-const log: Logger = config.createLogger('app');
 
 class Application {
   public initialize(): void {
@@ -13,50 +10,11 @@ class Application {
     const app: Express = express();
     const server: ChattyServer = new ChattyServer(app);
     server.start();
-    Application.handleExit();
   }
 
   private loadConfig(): void {
     config.validateConfig();
     config.cloudinaryConfig();
-  }
-
-  private static handleExit(): void {
-    process.on('uncaughtException', (error: Error) => {
-      log.error(`There was an uncaught error: ${error}`);
-      Application.shutDownProperly(1);
-    });
-
-    process.on('unhandleRejection', (reason: Error) => {
-      log.error(`Unhandled Rejection at promise: ${reason}`);
-      Application.shutDownProperly(2);
-    });
-
-    process.on('SIGTERM', () => {
-      log.error('Caught SIGTERM');
-      Application.shutDownProperly(2);
-    });
-
-    process.on('SIGINT', () => {
-      log.error('Caught SIGINT');
-      Application.shutDownProperly(2);
-    });
-
-    process.on('exit', () => {
-      log.error('Exiting');
-    });
-  }
-
-  private static shutDownProperly(exitCode: number): void {
-    Promise.resolve()
-      .then(() => {
-        log.info('Shutdown complete');
-        process.exit(exitCode);
-      })
-      .catch((error) => {
-        log.error(`Error during shutdown: ${error}`);
-        process.exit(1);
-      });
   }
 }
 
