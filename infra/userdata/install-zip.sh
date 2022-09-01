75870dc871c4b3dd48bd3a210fdbb33b19c8720b
uzochukwueddie <uzo.odozi@gmail.com>
2022-09-01 09:13:19 +0200

    updated install zip file

--- infra/userdata/install-zip.sh
+++ infra/userdata/install-zip.sh
@@ -5,9 +5,9 @@ echo $VPC_ID
 echo "***********VPC_ID***********"
 
 echo "This is the VPC ID - $VPC_ID" > text.txt
+apt-get install -y sudo
+ls -al /bin/sh && sudo rm /bin/sh && sudo ln -s /bin/bash /bin/sh && ls -al /bin/sh
 sudo apt-get install zip
-zip -v
-sudo apt install zip
 zip -v
 zip text.zip text.txt
 aws --region eu-central-1 s3 cp text.zip s3://chatapp-testing-app/develop/
