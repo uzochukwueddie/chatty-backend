0de2a5703af6dde9d3d4f723196d826c201d8f0a
uzochukwueddie <uzo.odozi@gmail.com>
2022-09-01 09:31:02 +0200

    update install zip

--- infra/userdata/install-zip.sh
+++ infra/userdata/install-zip.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
+function program_is_installed {
+  local return_=1
+
+  type $1 >/dev/null 2>&1 || { local return_=0; }
+  echo "$return_"
+}
+
 echo "***********VPC_ID***********"
 echo $VPC_ID
 echo "***********VPC_ID***********"
 
-echo "This is the VPC ID - $VPC_ID" > text.txt
-cat text.txt
-apk update
-apk add zip
+echo "This is the VPC ID - $VPC_ID" > text2.txt
+if [ $(program_is_installed zip) == 0 ]; then
+  apk update
+  apk add zip
+fi
 zip -v
-zip text.zip text.txt
-aws --region eu-central-1 s3 cp text.zip s3://chatapp-testing-app/develop/
-rm -rf text.txt
-rm -rf text.zip
+cat text2.txt
+zip -v
+zip text2.zip text2.txt
+aws --region eu-central-1 s3 cp text2.zip s3://chatapp-testing-app/develop/
+rm -rf text2.txt
+rm -rf text2.zip
